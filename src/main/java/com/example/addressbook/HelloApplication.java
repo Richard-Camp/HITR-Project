c062db58d49064a97d49e9abbbd06330495de3fb
Tauteranga <tauteranga@gmail.com>
2024-09-22 18:24:21 +1000

    user db

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -1,16 +1,18 @@
 package com.example.addressbook;
 
+import com.example.addressbook.model.User.SqliteUserDAO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     public static final String TITLE = "CareerLaunch";
     public static final int WIDTH = 1280;
-    public static final int HEIGHT = 720;
+    public static final int HEIGHT = 721;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
