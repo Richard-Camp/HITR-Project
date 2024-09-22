78f8a83685c9af48f41003d86fd2103ef698b6d5
Tauteranga <tauteranga@gmail.com>
2024-09-22 20:49:48 +1000

    login authentication

--- src/main/java/com/example/addressbook/controller/HelloController.java
+++ src/main/java/com/example/addressbook/controller/HelloController.java
@@ -1,20 +1,72 @@
 package com.example.addressbook.controller;
 
 import com.example.addressbook.HelloApplication;
+import com.example.addressbook.model.User.User;
+import com.example.addressbook.model.User.IUserDAO;
+import com.example.addressbook.model.User.SqliteUserDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
 import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class HelloController {
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private PasswordField passwordField;
 
+    private IUserDAO userDAO = new SqliteUserDAO();
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    protected void onLoginButtonClick(ActionEvent event) throws IOException {
+        String email = emailTextField.getText();
+        String password = passwordField.getText();
+
+        if (isLoginValid(email, password)) {
+
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+            stage.setScene(scene);
+        } else {
+            showAlert("Login Failed", "Invalid email or password.");
+        }
+    }
+
+    private boolean isLoginValid(String email, String password) {
+        User user = userDAO.getAllUsers()
+                .stream()
+                .filter(u -> u.getEmail().equals(email) && u.getPassword().equals(password))
+                .findFirst()
+                .orElse(null);
+        return user != null;
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @FXML
+    private Button homeButton;
+    @FXML
+    protected void onHomeButtonClick() throws IOException {
+        Stage stage = (Stage) homeButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
 
     @FXML
     private Hyperlink joinNowLink;
@@ -29,7 +81,7 @@ public class HelloController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/addressbook/signup.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root, 800, 600);
-            // Set the stage to be non-resizable
+
             signupStage.setResizable(true);
             signupStage.setScene(scene);
             signupStage.show();
@@ -56,27 +108,6 @@ public class HelloController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-
-    @FXML
-    private Button loginButton;
-    @FXML
-    protected void onLoginButtonClick() throws IOException {
-        Stage stage = (Stage) loginButton.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
-        stage.setScene(scene);
-    }
-
-    @FXML
-    private Button homeButton;
-    @FXML
-    protected void onHomeButtonClick() throws IOException {
-        Stage stage = (Stage) homeButton.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
-        stage.setScene(scene);
     }
 
 }
