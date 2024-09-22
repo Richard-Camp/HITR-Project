d4518994a8a85a96f4581f8302701a47cdecf110
PfizzahKhokhar <pfizzahzaheer@gmail.com>
2024-09-22 18:33:37 +1000

    login screen

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
     public static final int HEIGHT = 721;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("resume-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
